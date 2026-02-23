5cce692e2646f6feb4cb06e4d8f663d7db337b5b
Minerva Jiménez Álvarez <minerva.jimenez@nubexx.es>
2026-02-23 11:46:45 +0000

    feat: exercises 1 and 2

--- webapp/controller/main.controller.js
+++ webapp/controller/main.controller.js
@@ -1,12 +1,42 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
-function (Controller) {
-    "use strict";
+    function (Controller) {
+        "use strict";
 
-    return Controller.extend("com.nubexx.hr.pokemonfiori.controller.main", {
-        onInit: function () {
+        return Controller.extend("com.nubexx.hr.pokemonfiori.controller.main", {
+            onInit: function () {
 
-        }
+            },
+
+            onPress: function (oEvent) {
+                var buttonPressed = oEvent.getSource();
+                var id = buttonPressed.getId();
+                var language = this.getView().getModel("i18n").getResourceBundle();
+
+                var a = parseFloat(this.byId("input-a").getValue());
+                var b = parseFloat(this.byId("input-b").getValue());
+
+                if (isNaN(a) || isNaN(b)) {
+                    var errorText = language.getText("invalidDigits");
+
+                    sap.m.MessageToast.show(errorText);
+                    return;
+                }
+
+                var resultNumber;
+
+                if (id.includes("sumButton")) {
+                    resultNumber = a + b;
+                } else if (id.includes("subButton")) {
+                    resultNumber = a - b;
+                } else if (id.includes("multButton")) {
+                    resultNumber = a * b;
+                }
+
+                var resultText = language.getText("resultText")
+                sap.m.MessageToast.show(resultText + resultNumber);
+
+            }
+        });
     });
-});
