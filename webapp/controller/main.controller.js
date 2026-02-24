750878bb989b04b8add05dbce624bcfcaf595c50
Minerva Jiménez Álvarez <minerva.jimenez@nubexx.es>
2026-02-24 12:02:41 +0000

    actualizado

--- webapp/controller/main.controller.js
+++ webapp/controller/main.controller.js
@@ -1,12 +1,15 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    'sap/ui/model/json/JSONModel'
 ],
     function (Controller) {
         "use strict";
 
         return Controller.extend("com.nubexx.hr.pokemonfiori.controller.main", {
             onInit: function () {
-
+                //let oJsonModel = new sap.ui.model.json.JSONModel();
+                //oJsonModel.setProperty("/propiedad1", "1"); // el 1 es el valor
+                //this.getView().setModel(oJsonModel, "modelo1");
             },
 
             onPress: function (oEvent) {
